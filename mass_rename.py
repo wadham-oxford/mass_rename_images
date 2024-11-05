1c3725759b741680f967ed74ceac0bd28582c9e3
wadham-comms-2 <martin@wadham.ox.ac.uk>
2024-11-05 16:52:52 +0000

    Opens file explorer to selected path when finished.

--- mass_rename.py
+++ mass_rename.py
@@ -1,29 +1,43 @@
 import os
 from tkinter import filedialog
 
+
 def get_extension(string):
     dot = "."
     sub_strings = string.split(dot)
     extension = dot + sub_strings[-1]
     return extension
 
-print("\nSelect folder where you want to change file names.")
-folder_path = filedialog.askdirectory()
 
-content = input("\nWhat text would you like to all file names in this folder to include? ")
-
-file_list = os.listdir(folder_path)
-x = 1
-for file in file_list:
-    extension = get_extension(file)
-    original = os.path.join(folder_path, file)
+def get_number(x):
     if x < 10:
-        number = "0{}_".format(str(x))
+        return "0{}_".format(str(x))
     else:
-        number = "{}_".format(str(x))
-    new_file_name = number + content + extension
-    new_path = os.path.join(folder_path, new_file_name)
-    os.rename(original, new_path)
-    x += 1
+        return "{}_".format(str(x))
+    
 
-print("\nAll file names in {} are now renamed with structure 00_{}".format(folder_path, content))
+def change_names(folder_path, content):
+    file_list = os.listdir(folder_path)
+    x = 1
+    for file in file_list:
+        number = get_number(x)
+        extension = get_extension(file)
+        new_file_name = number + content + extension
+        new_path = os.path.join(folder_path, new_file_name)
+        original = os.path.join(folder_path, file)
+        os.rename(original, new_path)
+        x += 1
+
+
+if __name__ == "__main__":
+    print("\nSelect folder where you want to change file names.")
+    folder_path = filedialog.askdirectory()
+    print("\nFolder: {}".format(folder_path))
+
+    content = input("\nWhat text would you like all file names in this folder to include? ")
+    print("\nContent: {}".format(content))
+
+    change_names(folder_path, content)
+    print("\nAll file names changed.")
+
+    os.startfile(folder_path)
