25fd522889e7b0aef8f015b90144c8888cf43fec
Martin <martin.smith@wadham.ox.ac.uk>
2024-12-18 16:53:20 +0000

    added file extension list to repo.

---
+++ extensions.py
@@ -0,0 +1,3 @@
+IMAGE_EXTENSIONS = (".jpg", ".jpeg", ".png", ".gif", ".arw", ".svg", ".dng", 
+                    ".tiff", ".webp")
+VIDEO_EXTENSIONS = (".mp4", ".mov", ".avi", ".avchd", ".mkv", ".flv")
